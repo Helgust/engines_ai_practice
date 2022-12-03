6dfab7e6ebc8f8ab7d98f186d09bd2307255d529
Egor <egortutun@gmail.com>
2022-12-03 21:33:41 +0300

    Shitty SMA*

--- src/main.cpp
+++ src/main.cpp
@@ -118,10 +118,103 @@ static std::vector<Position> find_path_a_star(const char *input, size_t width, s
   return std::vector<Position>();
 }
 
+static std::vector<Position> find_path_sma_star(const char *input, size_t width, size_t height, 
+  Position from, Position to, int limit)
+{
+  if (from.x < 0 || from.y < 0 || from.x >= int(width) || from.y >= int(height))
+    return std::vector<Position>();
+  size_t inpSize = width * height;
+
+  std::vector<float> g(inpSize, std::numeric_limits<float>::max());
+  std::vector<float> f(inpSize, std::numeric_limits<float>::max());
+  std::vector<Position> prev(inpSize, {-1,-1});
+
+  auto getG = [&](Position p) -> float { return g[coord_to_idx(p.x, p.y, width)]; };
+  auto getF = [&](Position p) -> float { return f[coord_to_idx(p.x, p.y, width)]; };
+
+  auto heuristic = [](Position lhs, Position rhs) -> float
+  {
+    return sqrtf(square(float(lhs.x - rhs.x)) + square(float(lhs.y - rhs.y)));
+  };
+
+  g[coord_to_idx(from.x, from.y, width)] = 0;
+  f[coord_to_idx(from.x, from.y, width)] = heuristic(from, to);
+
+  std::vector<Position> openList = {from};
+  std::vector<Position> closedList;
+
+  while (!openList.empty())
+  {
+    size_t bestIdx = 0;
+    float bestScore = getF(openList[0]);
+    for (size_t i = 1; i < openList.size(); ++i)
+    {
+      float score = getF(openList[i]);
+      if (score < bestScore)
+      {
+        bestIdx = i;
+        bestScore = score;
+      }
+    }
+    if (openList[bestIdx] == to)
+      return reconstruct_path(prev, to, width);
+    Position curPos = openList[bestIdx];
+    openList.erase(openList.begin() + bestIdx);
+    if (std::find(closedList.begin(), closedList.end(), curPos) != closedList.end())
+      continue;
+    size_t idx = coord_to_idx(curPos.x, curPos.y, width);
+    DrawPixel(curPos.x, curPos.y, Color{uint8_t(g[idx]), uint8_t(g[idx]), 0, 100});
+    closedList.emplace_back(curPos);
+    auto checkNeighbour = [&](Position p)
+    {
+      // out of bounds
+      if (p.x < 0 || p.y < 0 || p.x >= int(width) || p.y >= int(height))
+        return;
+      size_t idx = coord_to_idx(p.x, p.y, width);
+      // not empty
+      if (input[idx] == '#')
+        return;
+      float weight = input[idx] == 'o' ? 10.f : 1.f;
+      float gScore = getG(curPos) + 1.f * weight; // we're exactly 1 unit away
+      if (gScore < getG(p))
+      {
+        prev[idx] = curPos;
+        g[idx] = gScore;
+        f[idx] = gScore + heuristic(p, to);
+      }
+      bool found = std::find(openList.begin(), openList.end(), p) != openList.end();
+      if (!found)
+        openList.emplace_back(p);
+
+      if ((openList.size() + closedList.size()) > limit)
+      {
+        size_t biggestIdx = 0;
+        float biggestScore = getF(openList[0]);
+        for (size_t i = 1; i < openList.size(); ++i)
+        {
+          float score = getF(openList[i]);
+          if (score > biggestScore)
+          {
+            biggestIdx = i;
+            biggestScore = score;
+          }
+        }
+        openList.erase(openList.begin() + biggestIdx);
+      }
+    };
+    checkNeighbour({curPos.x + 1, curPos.y + 0});
+    checkNeighbour({curPos.x - 1, curPos.y + 0});
+    checkNeighbour({curPos.x + 0, curPos.y + 1});
+    checkNeighbour({curPos.x + 0, curPos.y - 1});
+  }
+  // empty path
+  return std::vector<Position>();
+}
+
 void draw_nav_data(const char *input, size_t width, size_t height, Position from, Position to)
 {
   draw_nav_grid(input, width, height);
-  std::vector<Position> path = find_path_a_star(input, width, height, from, to);
+  std::vector<Position> path = find_path_sma_star(input, width, height, from, to, 1500);
   draw_path(path);
 }
 
